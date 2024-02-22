bab739f38545cd673e574441b6a3ac5a437d5ec9
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-02-22 18:11:20 +0100

    logo

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { cn } from "@/lib/utils"
+
+export const Navbar = () => {
+
+    return(
+        <div className={cn(
+            "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+            scrolled && "border-b shadow-sm"
+          )}>
+
+        </div>
+    )
+}

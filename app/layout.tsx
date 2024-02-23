ae89f056afc95ef48cc65eb9c9108ab6dccfbcf6
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-02-23 13:10:07 +0100

    database set

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ThemeProvider } from "@/components/Providers/theme-provider";
+import { ConvexClientProvider } from "@/components/Providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <ConvexClientProvider>
         <ThemeProvider
          attribute="class"
          defaultTheme="system"
@@ -42,6 +44,7 @@ export default function RootLayout({
         >
         {children}
         </ThemeProvider>
+        </ConvexClientProvider>
         </body>
     </html>
   );
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://https://well-sunfish-88.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
